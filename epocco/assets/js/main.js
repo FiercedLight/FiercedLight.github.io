a25231cb11466544285e800e9962e3b056db9d9e
Berkay Ayhan <e2089076@ceng.metu.edu.tr>
2018-12-26 18:43:53 +0300

    Epocco

--- epocco/assets/js/main.js
+++ epocco/assets/js/main.js
@@ -184,6 +184,74 @@
 
 		});
 
+	// Tiles2.
+	var $tiles2 = $('.tiles2 > article');
+
+	$tiles2.each(function() {
+
+		var $this = $(this),
+			$image = $this.find('.image'), $img = $image.find('img'),
+			$link = $this.find('.link'),
+			x;
+
+		// Image.
+
+			// Set image.
+				$this.css('background-image', 'url(' + $img.attr('src') + ')');
+
+			// Set position.
+				if (x = $img.data('position'))
+					$image.css('background-position', x);
+
+			// Hide original.
+				$image.hide();
+
+		// Link.
+			if ($link.length > 0) {
+
+				$x = $link.clone()
+					.text('')
+					.addClass('primary')
+					.appendTo($this);
+
+				$link = $link.add($x);
+
+				$link.on('click', function(event) {
+
+					var href = $link.attr('href');
+
+					// Prevent default.
+						event.stopPropagation();
+						event.preventDefault();
+
+					// Target blank?
+						if ($link.attr('target') == '_blank') {
+
+							// Open in new tab.
+								window.open(href);
+
+						}
+
+					// Otherwise ...
+						else {
+
+							// Start transitioning.
+								$this.addClass('is-transitioning');
+								$wrapper.addClass('is-transitioning');
+
+							// Redirect.
+								window.setTimeout(function() {
+									location.href = href;
+								}, 500);
+
+						}
+
+				});
+
+			}
+
+	});
+
 	// Header.
 		if ($banner.length > 0
 		&&	$header.hasClass('alt')) {
