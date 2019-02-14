d8dbbe3d47934f5e5340e8c259d15bf846f34b24
Berkay Ayhan <e2089076@ceng.metu.edu.tr>
2019-02-14 19:55:29 +0300

    Ballspin

---
+++ Ballspin/code0.js
@@ -0,0 +1,18 @@
+gdjs.GameCode = {};
+
+gdjs.GameCode.conditionTrue_0 = {val:false};
+gdjs.GameCode.condition0IsTrue_0 = {val:false};
+
+
+gdjs.GameCode.eventsList0xb0cf8 = function(runtimeScene) {
+
+}; //End of gdjs.GameCode.eventsList0xb0cf8
+
+
+gdjs.GameCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+gdjs.GameCode.eventsList0xb0cf8(runtimeScene);
+return;
+}
+gdjs['GameCode'] = gdjs.GameCode;
